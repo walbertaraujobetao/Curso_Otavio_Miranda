039dcd40bff254fb4054b82eb410f6960802deae
walbertaraujo <wap.beto@gmail.com>
2021-05-24 12:32:52 -0300

    ...

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Teste')
+#print('Teste')
+
+# nome = input('Qual seu nome: ')
+# idade = int(input('Qual sua idade: '))
+
+# idade_limite = 18
+
+# if idade >= idade_limite:
+#     print(f'{nome} seu empréstimo foi autorizado!')
+# else:
+#     print(f'{nome}seu empréstimo não autorizado!')
+
+texto = 'Python Estudos'
+
+print(texto[:6])
+    
+    
